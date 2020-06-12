ae99ff6009bdc6f1d1a1816cf49089ce5a794366
git-hgjeong <code.hgjeong@gmail.com>
2020-06-12 18:05:28 +0900

    add viewpager

--- app/src/main/java/com/example/examviewpager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/examviewpager/ui/home/HomeFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.example.examviewpager.R
+import com.example.examviewpager.ui.main.SectionsPagerAdapter
 
 class HomeFragment : Fragment() {
 
@@ -19,13 +18,10 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val viewPageAdapter = SectionsPagerAdapter(childFragmentManager)
+        val viewPager: ViewPager = root.findViewById(R.id.view_pager)
+        viewPager.adapter = viewPageAdapter
         return root
     }
 }
